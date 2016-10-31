fdce82578a17279451f1440005b366812a886781
Hiroaki Suzuki <hs2865@instance-1.c.capstonegs16.internal>
2016-10-31 02:41:01 +0000

    from google cloud

--- crawl/setting.py
+++ crawl/setting.py
@@ -4,12 +4,12 @@ Setting information for crawler
 from socket import gethostname
 
 class Setting:
-    crawler_index_file_path = 'C:\\Users\\HS\\Desktop\\school\\classes\\CAPSTONE\\system\\test_app\\file\\'
-    csv_path = 'C:\\Users\\HS\\Desktop\\school\\classes\\CAPSTONE\\system\\test_app\\csv\\'
+    crawler_index_file_path = '/home/gs/files/file/'
+    csv_path = '/home/gs/files/csv/'
     mongodb_host = '127.0.0.1'
     mongodb_port = 27017
     hostname = gethostname()
-    url_filepath_for_scrapy = 'C:\\Users\\HS\\Desktop\\school\\classes\\CAPSTONE\\system\\scrapy\\url\\'
+    url_filepath_for_scrapy = '/home/gs/files/scrapy/'
     url_filename_for_scrapy = 'url_list.txt'
     global_parsed_text_output = ''
 
