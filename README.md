c6161d37073e146755e9f6fec96498833660ba03
manuel <manuel@koracap.com>
2016-11-02 14:53:24 -0400

    new non-scrapy collector

--- README.md
+++ README.md
@@ -1,19 +1,17 @@
 # f16-GS
 
-### General Scrapy Instructions
-The following commands have to be run on the edgar directory.
-- To interactively scrape a website from the terminal: `scrapy shell %url%`.
-- To run the scraper script: `scrapy crawl edgar_spider -o netflix.json`.
+### New Scraper
+The scrapy-based web scraper has been dropped as the unstructured and inconsistent format of HTML documents make it inefficient and unreliable. The new scraper generates 3 data structures:
 
-To edit the scraper script, the main files are items.py (defines structure of output file) and edgar_spider.py.
-
-This is how I envision the first section (Item 1) will look like once we parse it.
+1. Dataframe with error logs.
+2. Dataframe with summary statistics of parsed data.
+3. JSON with data.
 
 ```
-{'number': 'Item 1',
- 'title': 'Business',
- 'content': [{
-    'ABOUT US': 'Netflix, Inc. (“Netflix”, “the Company”, “we”, or “us”) is the world’s leading Internet television network...',
-    'BUSINESS SEGMENTS': 'The Company has three reportable segments: Domestic streaming, International streaming and...'
+{'(doc_id, year)': [{
+  section_num :[
+    'Netflix, Inc. (“Netflix”, “the Company”, “we”, or “us”) is the world’s leading Internet television network...',
+    'The Company has three reportable segments: Domestic streaming, International streaming and...'
+    ]
  }]}
  ```
--- edgar/edgar/spiders/edgar_spider.py
+++ edgar/edgar/spiders/edgar_spider.py
@@ -14,13 +14,15 @@ from scrapy.exceptions import CloseSpider
 class edgarSpiderSpider(scrapy.Spider):
     name = "edgar_spider"
     allowed_domains = ["https://www.sec.gov"]
-    start_urls = ['https://www.sec.gov/Archives/edgar/data/1018724/000101872416000172/amzn-20151231x10k.htm', #Amazon
-                  'https://www.sec.gov/Archives/edgar/data/51143/000104746916010329/a2226548z10-k.htm', #IBM
-                  'https://www.sec.gov/Archives/edgar/data/37996/000003799616000092/f1231201510-k.htm', #Ford
-                  'https://www.sec.gov/Archives/edgar/data/895421/000119312516473553/d48186d10k.htm', #Morgan Stanley
-                  'https://www.sec.gov/Archives/edgar/data/1065280/000119312510036181/d10k.htm', #Netflix
-                  'https://www.sec.gov/Archives/edgar/data/1326801/000132680116000043/fb-12312015x10k.htm', #Facebook
-                  'https://www.sec.gov/Archives/edgar/data/320193/000119312513416534/d590790d10k.htm' #Apple
+    start_urls = ['https://www.sec.gov/Archives/edgar/data/1065280/000119312508040378/d10k.htm',
+                  'https://www.sec.gov/Archives/edgar/data/1065280/000119312509037430/d10k.htm',
+                  'https://www.sec.gov/Archives/edgar/data/1065280/000119312510036181/d10k.htm',
+                  'https://www.sec.gov/Archives/edgar/data/1065280/000119312511040217/d10k.htm',
+                  'https://www.sec.gov/Archives/edgar/data/1065280/000119312512053009/d260328d10k.htm',
+                  'https://www.sec.gov/Archives/edgar/data/1065280/000106528013000008/nflx1231201210kdoc.htm',
+                  'https://www.sec.gov/Archives/edgar/data/1065280/000106528014000006/nflx10k2013.htm',
+                  'https://www.sec.gov/Archives/edgar/data/1065280/000106528015000006/nflx201410k.htm',
+                  'https://www.sec.gov/Archives/edgar/data/1065280/000106528016000047/nflx201510k.htm'
                   ]
 
     def __init__(self,node=1,rep=1,*args,**kwargs):
@@ -40,12 +42,15 @@ class edgarSpiderSpider(scrapy.Spider):
 
     def parse_document(self, response):
         ## Define sections to extract.
-        sections = ['1', '1A', '1B', '2', '3', '4', '5', '6', '7', '7A', \
-        '8', '9', '9A', '9B', '10', '11', '12', '13', '14', '15']
+        #sections = ['1', '1A', '1B', '2', '3', '4', '5', '6', '7', '7A', \
+        #'8', '9', '9A', '9B', '10', '11', '12', '13', '14', '15']
+
+        sections = ['1A', '1B']
 
         ## Get company name and year.
         company = response.meta['url'].split('/')[6]
-        year = response.xpath("//text()[re:test(.,'For the (fiscal )?year')]").extract()[0].split(',')[-1]
+        year = response.xpath("//text()[re:test(., '\d{2}\, \d{4}')]")[0].extract().split(',')[-1].strip()
+        year = re.sub('\D','',year)
 
         if(len(company) < 0 or len(year) < 0):
             raise CloseSpider('Error extracting year of the document.')
@@ -62,11 +67,11 @@ class edgarSpiderSpider(scrapy.Spider):
         for i in range(len(sections)-1):
             #print 'Getting company %s, section %s.' %(company, sections[i])
             ## Define start and ending paths. The problem is that many documents have different CSS formats...
-            start = "(preceding::font[re:test(text(),'ITEM\s" + sections[i] + "\. ')] | preceding::b[re:test(text(),'Item\s" + sections[i] + "\.')] | preceding::font[contains(@style,'font-weight:bold') and re:test(text(),'Item\s" + sections[i] + "\.')])"
+                start = "(preceding::font[re:test(text(),'ITEM\s" + sections[i] + "\. ')] | preceding::b[re:test(text(),'Item\s" + sections[i] + "\.')] | preceding::font[contains(@style,'font-weight:bold') and re:test(text(),'Item\s" + sections[i] + "\.')] | preceding::b[re:test(.,'Item\s" + sections[i] + "\.')])"
 
-            end = "(following::font[re:test(text(),'ITEM\s" + sections[i+1] + "\. ')] | following::b[re:test(text(),'Item\s" + sections[i+1] + "\.')] | following::font[contains(@style,'font-weight:bold') and re:test(text(),'Item\s" + sections[i+1] + "\.')])"
+                end = "(following::font[re:test(text(),'ITEM\s" + sections[i+1] + "\. ')] | following::b[re:test(text(),'Item\s" + sections[i+1] + "\.')] | following::font[contains(@style,'font-weight:bold') and re:test(text(),'Item\s" + sections[i+1] + "\.')]  | preceding::b[re:test(.,'Item\s" + sections[i+1] + "\.')])"
 
-            content = response.xpath("//text()[string-length() > 50 and " + start  + " and " + end + "]").extract()
+                content = response.xpath("//text()[string-length() > 75 and " + start  + " and " + end + "]").extract()
 
             #start = "(b[contains(text(), 'Item') and contains(text(), '" + sections[i] + ".') and text()[string-length() < 7 + " + str(len(sections[i])) + "]] | \
             #         //font[contains(@style, 'weight:bold') and (text() = 'Item " + sections[i] + ".' or text() = 'ITEM " + sections[i] + ". ')])"
