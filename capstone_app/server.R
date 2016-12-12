1e7d8e2b8d0f3a991ba6b95655a5e3b1eb38b11d
Manuel Rueda <manolorueda@gmail.com>
2016-12-12 16:08:37 -0500

    final app

--- capstone_app/server.R
+++ capstone_app/server.R
@@ -7,21 +7,66 @@ library(highcharter)
 library(reshape)
 library(stringr)
 library(broom)
-library(zoo)   
-
 
 #setwd('/home/hs2865/src/f16-GS/capstone_app')
 setwd('/Users/manuelrueda/Dropbox/Documents/python/f16-GS/capstone_app')
-data <- read.csv("results3.csv")
-colnames(data) <- c('CIK', 'Jaccard', 'TF-IDF', 'Word2Vec', 'Common Words', 'Deleted Words','New Words', 'Total Words', 'Top Words', 'Year')
-data[,c(2,3,4,5)] = round(data[,c(2,3,4,5)],2)
-companies = read.csv('company_list.csv', stringsAsFactors=F)
 
-all_years = c(2008,2009,2010,2011,2012,2013,2014,2015)
+## Read data.
+companies <- read.csv('cik_new.csv', header=FALSE, sep='|', stringsAsFactors = FALSE)
+colnames(companies) <- c('Company.Name', 'CIK') 
+companies <- companies[!duplicated(companies[,'CIK']),]
+data <- read.csv("results-best.csv")
+
+## Clean and format.
+data <- unique(data)
+colnames(data) <- c('CIK', 'Jaccard', 'TF-IDF', 'Word2Vec', 'Common Words', 'Deleted Words','New Words', 'Total Words', 'Top Words', 'Year')
+data[,c(2:7)] = round(data[,c(2:7)],2)
+data$CIK <- sprintf('%010d', data$CIK)
+
 data <- merge(x = data, y = companies[,c('CIK', 'Company.Name')], by = "CIK", all.x = TRUE)
 
+## Normalize word2vec.
+data$Word2Vec <- round(data$Word2Vec / max(data$Word2Vec),2)
+## Get means.
+means <- round(colMeans(data[,c('Jaccard','TF-IDF','Word2Vec')]),2)
+## Get IDs.
+#mapping <- seq(length(unique(data$Company.Name)))
+#names(mapping) <- unique(data$Company.Name)
+#data$Id <- mapping[data$Company.Name]
+
+## Filled heatmap dataset.
+all_years = c(2008,2009,2010,2011,2012,2013,2014,2015)
+all_companies <- unique(data$CIK)
+#filled_data <- data[, c('Year', 'Id', 'CIK', 'Company.Name', 'Jaccard', 'TF-IDF', 'Word2Vec')]
+filled_data <- data[, c('Year', 'CIK', 'Company.Name', 'Jaccard', 'TF-IDF', 'Word2Vec')]
+
+for(company in all_companies) {
+  tmp_data <- data[data$CIK == company, c('Year', 'CIK', 'Company.Name', 'Jaccard', 'TF-IDF', 'Word2Vec')]
+  missing_years = setdiff(all_years, unique(tmp_data$Year))
+  name <- unique(tmp_data$Company.Name)
+  cik <- unique(tmp_data$CIK)
+  for(year in missing_years) {
+    new_row = c(year, cik, name, means[1], means[2], means[3])
+    filled_data = rbind(filled_data, new_row)
+  }
+}
+filled_data <- arrange(filled_data, CIK, Year)
+update.cols <- c('Year','Jaccard', 'TF-IDF', 'Word2Vec')
+filled_data[update.cols] <- sapply(filled_data[update.cols],as.numeric)
+
+
+## Get IDs.
+mapping <- seq(length(unique(filled_data$Company.Name)))
+names(mapping) <- unique(filled_data$Company.Name)
+filled_data$Id <- mapping[filled_data$Company.Name]
+data$Id <- mapping[data$Company.Name]
+
+
+## Get list of all companies and CIKs.
+cik_map <- unique(data[c('CIK','Company.Name')])
+
 shinyServer(function(input, output, session) {
-  values <- reactiveValues(company = '1053352')
+  values <- reactiveValues(company = '0000005981')
   # Fill in the spot we created for a plot
   observeEvent(input$go, {
     values$company <- input$cik
@@ -78,6 +123,7 @@ shinyServer(function(input, output, session) {
   output$plot1 <- renderHighchart({
     plot1 <- highchart() %>% 
       hc_chart(type = "column") %>% 
+      hc_yAxis(max = 1) %>%
       #hc_yAxis(title = list(text = "Weights")) %>% 
       # hc_plotOptions(column = list(
       #   dataLabels = list(enabled = FALSE),
@@ -85,8 +131,8 @@ shinyServer(function(input, output, session) {
       #   enableMouseTracking = FALSE)
       # ) %>% 
       hc_xAxis(categories = distance()$Year) %>% 
-      hc_add_series(name="TF-IDF", data=distance()$`TF-IDF`) %>%
       hc_add_series(name="Jaccard", data=distance()$Jaccard) %>%
+      hc_add_series(name="TF-IDF", data=distance()$`TF-IDF`) %>%
       hc_add_series(name="Word2Vec", data=distance()$Word2Vec)
     plot1
   })
@@ -123,11 +169,44 @@ shinyServer(function(input, output, session) {
       hc_plotOptions(column = list(stacking = "normal")) %>%
       hc_xAxis(categories = changes()$Year) %>% 
       hc_yAxis(max = 1) %>%
-      hc_add_series(name="Common Words", data=changes()$`Common Words`) %>%
-      hc_add_series(name="New Words", data=changes()$`New Words`) %>%
-      hc_add_series(name="Deleted Words", data=changes()$`Deleted Words`)
+      hc_add_series(name="Deleted Words", data=changes()$`Deleted Words`, color='#444349') %>%
+      hc_add_series(name="New Words", data=changes()$`New Words`, color='#A4EA8A') %>%
+      hc_add_series(name="Common Words", data=changes()$`Common Words`, color='#86B4E6')
     plot2
   })
+  
+  output$heatmap <- renderHighchart({
+    heatmap <- highchart() %>%
+      hc_chart(type = "heatmap")
+      #hc_title(text = "Simulated values by years and months") %>%
+      #hc_xAxis(categories = data$Company.Name) %>%
+    if(input$map.type == 'Jaccard') {
+      heatmap <- heatmap %>%
+        hc_add_series(name = "Jaccard", data = list_parse2(filled_data[,c('Year','Id','Jaccard')]))
+    } else if(input$map.type == 'TF-IDF') {
+      heatmap <- heatmap %>%
+        hc_add_series(name = "TF-IDF", data = list_parse2(filled_data[,c('Year','Id','TF-IDF')]))
+    } else if (input$map.type == 'Word2Vec') {
+      heatmap <- heatmap %>%
+        hc_add_series(name = "Word2Vec", data = list_parse2(filled_data[,c('Year','Id','Word2Vec')]))
+    }
+    heatmap <- heatmap %>%
+      hc_yAxis(categories = c('CIK', names(mapping))) %>%
+      hc_colorAxis(minColor = "#ffd3b6", maxColor = "#ff8b94")
+    
+    heatmap
+  })
+  
+  
+  #output$heatmap2 <- renderHighchart({
+  #  heatmap <- highchart() %>%
+  #    hc_chart(type = "heatmap") %>%
+  #    #hc_title(text = "Simulated values by years and months") %>%
+  #    #hc_xAxis(categories = data$Company.Name) %>%
+  #    hc_yAxis(categories = c('Company.Name', names(mapping))) %>%
+  #    hc_add_series(name = "Word2Vec", data = list_parse2(data[,c('Year','Id','Word2Vec')]))
+  #  hc_colorAxis(heatmap, minColor = "#ffeda0", maxColor = "#f03b20")
+  #})  
   
   #output$plot2 <- renderPlot({
   # output$plot2 <- renderChart2({
@@ -174,6 +253,10 @@ shinyServer(function(input, output, session) {
     DT::datatable(data[,c('CIK', 'Year','Jaccard', 'TF-IDF', 'Deleted Words','New Words', 'Top Words')])
   )
   
+  output$tab3 <- DT::renderDataTable(
+    DT::datatable(cik_map)
+  )
+  
   output$tit <- renderText(
     dataset()[dataset()$CIK == values$company, 'Company.Name'][1]
   )
@@ -181,3 +264,4 @@ shinyServer(function(input, output, session) {
   ## Send plots to UI.
   #plot2 %>% bind_shiny("word_dist")
 })
+
--- capstone_app/ui.R
+++ capstone_app/ui.R
@@ -17,16 +17,43 @@ dashboardPage(
   
   dashboardSidebar(
     sidebarMenu(
+      menuItem("Overview", tabName = "overview", icon = icon("book")),
       menuItem("Analysis", tabName = "analysis", icon = icon("bar-chart")),
-      menuItem("All Data", tabName = "allData", icon = icon("table"))
+      menuItem("Heatmap", tabName = "heatmap", icon = icon("table"))
     ),
     ## Filtering items.
-    textInput('cik', "Enter CIK", '37996'),
+    textInput('cik', "Enter CIK", '0000005981'),
     actionButton('go', 'Go!')
   ),
   
   dashboardBody(
     tabItems(    
+      tabItem(tabName = 'overview',
+              h1('Quantifying Changes in Financial Reports'),
+              fluidRow(
+                column(10,
+                        p('The United States Securities and Exchange Commission (SEC) requires that companies
+                          with more than $10MM in assets and more than 500 owners of a class of equity securities
+                          must file annual and quarterly reports that provide a comprehensive summary of their financial performance.
+                          These annual reports are known as Form 10-K and Form 10-Q respectively. Information in Form 10-K includes,
+                          but is not limited to, company history, organizational structure, executive compensation, risk factors
+                          and auditedfinancial statements. Form 10-Q contains similar, though less detailed, information.
+                          Most notably financial statements in the 10-Q are typically unaudited. Firms rarely make significant changes
+                          to the language and construction of their 10-Ks and 10-Qs; usually they simply repeat the information
+                          and language used to convey such information from one report to the next.
+                          Only a small percentage of companies make large changes to their reports on a quarterly or annual basis.
+                          Given this tendency, when a firm does break with their reporting routine, it may signal important information
+                          about future financial performance.'),
+                       p(' .'),
+                       p(' .')
+                       ),
+                fluidRow(
+                  column(6,
+                       DT::dataTableOutput('tab3')
+                       )
+                )
+                )
+              ),
       tabItem(tabName = "analysis",
                 h1(strong(textOutput('tit'))),
                 ## Tab Item 1: Charts.
@@ -60,11 +87,17 @@ dashboardPage(
                 )
                 
         ),
-        tabItem(tabname = 'allData',
+        tabItem(tabName = 'heatmap',
+                h2('Overview of All Companies'),
+                radioButtons("map.type", label = p("Measure"),  inline=TRUE, 
+                             choices = list("Jaccard" = "Jaccard", "TF-IDF" = "TF-IDF", "Word2Vec" = "Word2Vec"),
+                             selected = "TF-IDF"),
                 fluidRow(
-                  column(12,
-                         h2(strong("All Data")),
-                         DT::dataTableOutput('tab2')
+                  column(6,
+                         highchartOutput("heatmap",height = "800px")
+                  ),
+                  column(6,
+                         highchartOutput("heatmap2",height = "800px")
                   )
                 )
               )
